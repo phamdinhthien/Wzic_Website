ba45f900a6fe8b7fa55f6adfe0cc3e1db420f35d
phamdinhthien <phamdinhthien99@gmail.com>
2019-03-09 00:08:28 +0700

    ok

---
+++ Wzic/Wzic/Scripts/artists.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+$(document).ready(function(e) {
+      var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 500,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+	   navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+	exit.click(function(e) {
+        box.css('display', 'none');
+    });
+	
+});
---
+++ Wzic/Wzic/Scripts/home.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function (e) {
+    video.click(function (e) {
+        header.css('display', 'none');
+        menuBar.css('display', 'none');
+    })
+});
+
+var header = $('.header');
+var menuBar = $('.menuBar');
+var video = $('i.fa-viadeo a')
